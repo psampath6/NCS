b1e7ce46d522231336bcc0526e1ce57443e08cc8
Pradeep Sampath <pradeepsampath@gmail.com>
2021-03-06 23:35:37 -0800

    fix

--- bs-hm-ad.php
+++ bs-hm-ad.php
@@ -9,34 +9,54 @@
 */
 
 ?>
+<?php
 
+  if(!isset($_SESSION['SESSION_NKPUID']))
+  {
+    header("Location: login.php");
+    exit;
+  }
+
+
+  $vsNkpUid = $_SESSION['SESSION_NKPUID'];
+
+  $vsEmailid = $_SESSION['SESSION_EMAILID'];
+
+  $vsFirstName = $_SESSION['SESSION_FIRSTNAME'];
+
+  $vsLastName = $_SESSION['SESSION_LASTNAME'];
+
+?>
 <style>
 </style>
 
 	<!--
 	-->
+	<li class="dropdown">
+			<a id="NbLogout" href="#" class="dropdown-toggle" data-toggle="dropdown"
+						title="<?php echo ucwords($vsFirstName) . ' ' . ucwords($vsLastName); echo ' <' . strtolower($vsEmailid) . '>'; ?>">
+						<?php
+							echo ucwords($vsFirstName) ;
+						?>
+					 <b class="caret"></b>
+			</a>
 
-		<li class="dropdown">
-			<a class="dropdown-toggle" data-toggle="dropdown" style="cursor:pointer;" >
-				<i class="fa fa-user"></i>&nbsp; Admin <b class="caret"></b>&nbsp;</a>
-			<ul class="dropdown-menu alert-dropdown0">
-				<li class="divider"></li>
+			<ul class="dropdown-menu">
 				<li>
-						<a style="cursor:default;" >
-						<div class="label label-default" style="font-size:14px" >
-							Pradeep Sampath
-						</div></a>
+					<a href="#" style="cursor: default;">
+						<?php
+						echo ucwords($vsFirstName) . ' ' . ucwords($vsLastName) ;
+						echo ' <br>' ;
+						echo strtolower($vsEmailid);
+						?>
+						<br>
+						<span style="font-size:5px">-------------------------</span>
+					</a>
 				</li>
 				<li>
-						<a href="mailto:pradeepsampath@gmail.com" target="_top" style="" >
-						<div class="label label-info" style="font-size:14px" >
-							<i class="fa fa-fw fa-envelope"></i>
-							pradeepsampath@gmail.com
-						</div></a>
+					<a href="logout.php" class="NbLogout">
+						<b>Logout</b>
+					</a>
 				</li>
-				<li class="divider"></li>
 			</ul>
-		</li>
-
-	<!--
-	-->
+	</li>
